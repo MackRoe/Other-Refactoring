31ea343d6f4881ec82635fc5f147b4a8a6398ffa
MackRoe <elaine.music@students.makeschool.com>
2021-02-23 10:20:52 -0500

    makes notes about refactor for extract_superclass2.py

--- extract_superclass2.py
+++ extract_superclass2.py
@@ -1,18 +1,29 @@
-# by Kami Bigdely
-# Extract superclass.
+# Starter Code by Kami Bigdely
+# Goal: Extract superclass.
+# Refactor by Laine (Music)
+
+# Superclass: Shape
+#   Attributes: x, y, visible
+#   Methods: toggle_visible()
+#            display_shape()
+
+
 class Circle:
     
     def __init__(self, x, y, r, visible = True):
+      # Should the value of the visible variable be set in the arguments?
       self.center_x = x
       self.center_y = y
-      self.r = r
+      self.r = r  # Unclear variable naming
       self.visible = visible
+
       
     def display(self):
         print('drew the circle.')
         
     def set_visible(self,is_visible):
         self.visible = is_visible
+        # Seems redundent. Why are we re-assigning this?
         
     def get_center(self):
         return self.center_x, self.center_y
@@ -21,7 +32,7 @@ class Circle:
 class Rectangle:
     
     def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
+        ''' Returns the coordinates of the left-bottom corner. ''' 
         self.x = x
         self.y = y
         self.width = width
@@ -37,6 +48,7 @@ class Rectangle:
         
     def make_visible(self):
         self.visible = True
+        # redundant
         
     def get_center(self):
         return self.x + self.width/2, \
@@ -48,11 +60,11 @@ if __name__ == "__main__":
     circle = Circle(0,0,10, False)
     circle.set_visible(True)
     circle.display()
-    print('center point',circle.get_center())
+    print('center point: ', circle.get_center())
 
     rect = Rectangle(10, 10, 20, 5)
     rect.hide()
-    rect.display() # does not display because it's hidden.
+    rect.display()  # does not display because it's hidden.
     rect.make_visible()
     rect.display()
-    print('center point',rect.get_center())
+    print('center point: ', rect.get_center())
